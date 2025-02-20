e6aad73e3163000ad9d2f7ba61786f9625603309
srjvshelke <surajshelkeofficial@gmail.com>
2025-02-20 18:09:10 +0530

    front

--- server/dist/routes/authRoutes.js
+++ server/dist/routes/authRoutes.js
@@ -58,4 +58,62 @@ router.post('/register', async (req, res) => {
         }
     }
 });
+router.post('/login', async (req, res) => {
+    try {
+        const body = req.body;
+        const payload = registerSchema.parse(body);
+        console.log(payload);
+        let user = await register.findOne({
+            email: payload.email
+        });
+        console.log(user);
+        if (user == null || !user) {
+            return res
+                .status(404)
+                .json({ message: "No user found with this email." });
+        }
+        // * Check email verified or not
+        if (user.email_verified_at === null) {
+            return res.status(422).json({
+                errors: {
+                    email: "Email is not verified yet.please check your email and verify your email.",
+                },
+            });
+        }
+        //encrypt password
+        const salt = await bcrypt.genSalt(10);
+        payload.password = await bcrypt.hash(payload.password, salt);
+        payload.confirm_password = payload.password;
+        const id = generateRandomNum();
+        const token = await bcrypt.hash(id, salt);
+        const url = `${process.env.APP_URL}/verify-email/?email=${payload.email}&token=${token}`;
+        const html = await renderEmailEjs("verify-email", {
+            name: payload.name,
+            url: url,
+        });
+        console.log(url);
+        await emailQueue.add(emailQueueName, {
+            to: payload.email,
+            subject: "Please verify your email Clash",
+            body: html,
+        });
+        Object.assign(payload, { email_verify_token: token });
+        await register.create(payload);
+        return res.json({ message: "User created successfully!" });
+    }
+    catch (error) {
+        console.log(req.body);
+        console.log("The error is ", error);
+        if (error instanceof ZodError) {
+            const errors = formatError(error);
+            res.status(422).json({ message: "Invalid data", errors });
+        }
+        else {
+            // logger.error({ type: "Register Error", body: JSON.stringify(error) });
+            res
+                .status(500)
+                .json({ error: "Something went wrong.please try again!", data: error });
+        }
+    }
+});
 export default router;
